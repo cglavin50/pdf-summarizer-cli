793b6e15ebe1d8eb2c3a863f6953b8ab0cae26c9
cglavin50 <cooperglavin@gmail.com>
2023-07-15 16:25:47 -0400

    Switched to map reduce summarizing technique

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 A CLI tool written in Python to summarize PDF's text. Showcases LangChains map-reduce and refine chaining schemes to summarize longer pieces of text when stuffing isn't an option.
 
-## To Do
+Currently, there is code in the modelHandler.py file to use both methods. As seen in the examples, Map Reduce creates longer, but arguably stronger, summaries, and is significantly faster, so that is what is currently being used (but can be modified). This repository can be configured to demonstrate both applications, and test them for yourself.
 
-- Compare run times with parallelization in divide-and-conquer strategy
+Running `$ python3 main.py` will print a help message with all the required command-line args for use.
--- handlers/modelHandler.py
+++ handlers/modelHandler.py
@@ -1,9 +1,14 @@
-from langchain.chains import RefineDocumentsChain, LLMChain
+from langchain.chains import RefineDocumentsChain, StuffDocumentsChain, LLMChain, ReduceDocumentsChain, MapReduceDocumentsChain
 from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate
 from langchain.prompts import PromptTemplate
 import os
 
+
+document_prompt = PromptTemplate(
+    input_variables=["page_content"],
+     template="{page_content}"
+) # for formatting documents
 prompt_template = """Please summarize the following text:\n{text}"""
 prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
 refine_template = (
@@ -14,19 +19,60 @@ refine_template = (
     "---------\n"
     "{text}"
     "---------\n"
-    "Given this additional context, refine the original summary, taking care to avoid recency bias."
+    "Given this additional context, refine the original summary, weighing the original summary more than the next piece to avoid recency bias between inputs."
     "If the newly provided context isn't useful in relation to the subject matter, please return the original summary."
 )
 refine_prompt = PromptTemplate(
     input_variables=["prev_answer", "text"],
     template = refine_template
 )
+reduce_template = (
+    "Given smaller summaries from different parts of a larger piece of work,"
+    " please combine them, creating one cohesive summary."
+    "These summaries are all related, so make sure to connect contexts between them."
+    "The summaries are below:"
+    "---------\n"
+    "{text}"
+    "---------\n"
+)
+reduce_prompt = PromptTemplate(
+    input_variables=["text"],
+    template = reduce_template
+)
 
 
 
 def summarizeText(docs): # takes in an array of documents
     llm = ChatOpenAI(model_name = "gpt-3.5-turbo", openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.3)
-        
+
+    return mapReduceSummary(docs, llm)
+    
+    
+def mapReduceSummary(docs, llm):
+    llm_chain = LLMChain(llm=llm, prompt = prompt)
+    reduce_llm_chain = LLMChain(llm = llm, prompt = reduce_prompt)
+    combine_documents_chain = StuffDocumentsChain(
+        llm_chain = reduce_llm_chain,
+        document_prompt=document_prompt,
+        document_variable_name="text",
+    )
+    reduce_documents_chain = ReduceDocumentsChain(
+        combine_documents_chain=combine_documents_chain,
+    ) # breaks the documents down recursively, formats using the document prompt, and combines them using the StuffDocumentsChain
+    
+    chain = MapReduceDocumentsChain(
+        llm_chain=llm_chain,
+        reduce_documents_chain=reduce_documents_chain,
+        verbose=True,
+    )
+    
+    result = chain(
+        inputs = docs,
+        return_only_outputs = True,
+    )
+    return result["output_text"]  
+    
+def refineSummary(docs, llm):
     llm_chain = LLMChain(llm=llm, prompt = prompt)
     llm_chain_refine = LLMChain(llm=llm, prompt=refine_prompt)
     chain = RefineDocumentsChain(
@@ -42,4 +88,5 @@ def summarizeText(docs): # takes in an array of documents
         return_only_outputs = True,
     )
     return result["output_text"]
+# using the refine method
  
